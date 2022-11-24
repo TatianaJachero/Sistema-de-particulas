90f66443b41065a0f31a7b0571eb0d0a6821dd99
Tatiana <pamepachojb@hotmail.com>
2022-11-24 18:10:44 -0500

    Se crearon circulos y se asignaron sistemas

--- sketch.js
+++ sketch.js
@@ -1,7 +1,51 @@
+let nPelotas = 100;
+let pelotas = [];
+let t = 0;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  for (let i = 0; i < nPelotas; i++) {
+    pelotas[i] = new RandomWalek(i);
+  }
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+  for (let i = 0; i < nPelotas; i++) {
+    pelotas[i].update(t);
+    pelotas[i].display();
+  }
+  t += 0.01;
 }
+
+// _________________________________
+// ______Classes____________________
+// _________________________________
+
+
+// ______Random walker______________
+
+class RandomWalek {
+  constructor(_name) {
+    this.red = random(0, 50);
+    this.green = random(100, 255);
+    this.blue = random(100, 150);
+
+    this.pos = createVector(width / 2, height / 2);
+    this.speed = createVector(random(-5, 5), random(-5, 5));
+    this.diametro = random(10, 50);
+    print('Hola! soy la pelota' + this.name);
+  }
+
+  update(_t) {
+
+    this.speed.rotate(map(noise(_t), 0, 1, -0.5, 0.5));
+    this.pos.add(this.speed);
+  }
+
+  display() {
+    fill(this.red, this.green, this.blue);
+    ellipse(this.pos.x, this.pos.y, this.diametro, this.diametro);
+
+  }
+}
+
