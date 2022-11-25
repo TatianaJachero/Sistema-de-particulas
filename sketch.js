b0213d4d6768b8279095dd252c8a9d7ab982ffe3
Tatiana <pamepachojb@hotmail.com>
2022-11-24 20:36:56 -0500

    Antes de agregar nueva variente para posicion circulos

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
-let nTriangle = 120;
-let pelotas = [];
+let nTriangle = 60;
+let triangulo = [];
 let t = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < nTriangle; i++) {
-    pelotas[i] = new RandomWalek(i);
+    triangulo[i] = new RandomWalek(i);
   }
 }
 
 function draw() {
   for (let i = 0; i < nTriangle; i++) {
-    pelotas[i].update(t);
-    pelotas[i].display();
+    triangulo[i].update(t);
+    triangulo[i].display();
   }
   t += 0.005;
 }
@@ -34,16 +34,15 @@ class RandomWalek {
     this.tSpeed = random(1);
 
 
-    this.noiseShift = random(100);
+    this.noiseShift = random(1000);
 
     this.pos = createVector(width / 2, height / 2);
     this.pos2 = createVector(width / 3, height / 3);
-    this.pos3 = createVector(width / 4, height / 4);
+    this.pos3 = createVector(width / 2, height / 2);
     this.speed = createVector(random(-3, 3), random(-3, 3));
 
     this.pos4 = createVector(width / 2, height / 2);
-    this.pos5 = createVector(width / 2, height / 2);
-    this.pos6 = createVector(width / 5, height / 5);
+    this.diametro = random(10, 30);
     this.speed = createVector(random(-5, 5), random(-5, 5));
 
 
@@ -53,21 +52,21 @@ class RandomWalek {
 
   update(_t) {
 
-    this.speed.rotate(map(noise(this.t), 0, 1, -0.5, 0.5));
+    this.speed.rotate(map(noise(this.t + this.noiseShift), 0, 1, -0.5, 0.5));
     this.pos.add(this.speed);
 
     this.t += this.tSpeed;
   }
 
   display() {
-    stroke('rgba*255,150,0,0.1)');
+    stroke('rgba(255,150,0,0.5)');
     strokeWeight(1.5);
     fill(this.red, this.green, this.blue);
     triangle(this.pos.x, this.pos.y, this.pos2.x, this.pos2.y, this.pos3.x, this.pos3.y);
 
-    stroke('rgba*255,15,110,0.01)');
+    stroke('rgba(255,15,110,0.01)');
     fill(this.red, this.green, this.blue);
-    triangle(this.pos4.x, this.pos4.y, this.pos5.x, this.pos5.y, this.pos6.x, this.pos6.y);
+    ellipse(this.pos4.x, this.pos4.y, this.diametro, this.diametro);
 
   }
 }
