196473b9ea449d87b8ce307a6e0ed586668b81c6
Tatiana <pamepachojb@hotmail.com>
2022-11-24 21:12:43 -0500

    Final proyecto
    aun probare con otras figuras

--- sketch.js
+++ sketch.js
@@ -1,20 +1,31 @@
-let nTriangle = 60;
-let triangulo = [];
+let nTriangle = 80;
+let triangulos1 = [];
+let triangulos2 = [];
 let t = 0;
+let t2 = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < nTriangle; i++) {
-    triangulo[i] = new RandomWalek(i);
+    triangulos1[i] = new RandomWalek(i);
   }
+  for (let i = 0; i < nTriangle; i++) {
+    triangulos2[i] = new RandomWalek(i);
+  }
+  background('rgba(150,0,30,0.2)')
 }
 
 function draw() {
   for (let i = 0; i < nTriangle; i++) {
-    triangulo[i].update(t);
-    triangulo[i].display();
+    triangulos1[i].update(t);
+    triangulos1[i].display();
   }
-  t += 0.005;
+  for (let i = 0; i < nTriangle; i++) {
+    triangulos2[i].update(t);
+    triangulos2[i].display();
+  }
+  t += 0.01;
+  t2 += 0.0001;
 }
 
 // _________________________________
@@ -26,15 +37,23 @@ function draw() {
 
 class RandomWalek {
   constructor(_name) {
-    this.red = random(70, 250);
-    this.green = random(0, 255);
-    this.blue = random(150, 150);
+    this.red = random(100, 250);
+    this.green = random(0, 25);
+    this.blue = random(10, 150);
+
+    this.red2 = random(100, 250);
+    this.green2 = random(10, 255);
+    this.blue2 = random(50, 150);
 
     this.t = 0;
     this.tSpeed = random(1);
 
+    this.t2 = 0;
+    this.tSpeed2 = random(1);
 
-    this.noiseShift = random(1000);
+
+    this.noiseShift = random(2000);
+    this.noiseShift2 = random(1000);
 
     this.pos = createVector(width / 2, height / 2);
     this.pos2 = createVector(width / 3, height / 3);
@@ -42,8 +61,9 @@ class RandomWalek {
     this.speed = createVector(random(-3, 3), random(-3, 3));
 
     this.pos4 = createVector(width / 2, height / 2);
-    this.diametro = random(10, 30);
-    this.speed = createVector(random(-5, 5), random(-5, 5));
+    this.pos5 = createVector(width / 2, height / 2);
+    this.pos6 = createVector(width / 5, height / 5);
+    this.speed2 = createVector(random(-5, 5), random(-5, 5));
 
 
 
@@ -56,17 +76,23 @@ class RandomWalek {
     this.pos.add(this.speed);
 
     this.t += this.tSpeed;
+
+    this.speed2.rotate(map(noise(this.t2 + this.noiseShift2), 0, 1, -0.5, 0.5));
+    this.pos4.add(this.speed2);
+
+    this.t2 += this.tSpeed2;
   }
 
   display() {
-    stroke('rgba(255,150,0,0.5)');
-    strokeWeight(1.5);
+    //se agrega a proposito una llave en stroke para generar un borde blanco
+    stroke('rgba}25,15,0,0.5)');
+    strokeWeight(1);
     fill(this.red, this.green, this.blue);
     triangle(this.pos.x, this.pos.y, this.pos2.x, this.pos2.y, this.pos3.x, this.pos3.y);
 
-    stroke('rgba(255,15,110,0.01)');
-    fill(this.red, this.green, this.blue);
-    ellipse(this.pos4.x, this.pos4.y, this.diametro, this.diametro);
+    stroke('rgba]0,100,150,0.01)');
+    fill(this.red2, this.green2, this.blue2);
+    triangle(this.pos4.x, this.pos4.y, this.pos5.x, this.pos5.y, this.pos6.x, this.pos6.y);
 
   }
 }
